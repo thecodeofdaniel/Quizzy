3f808bcba6f2860e4a5497a695a8fc0561cfb791
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-04-07 01:32:27 -0700

    Fetch categories to display to user

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,34 @@
-const takeQuizBtn = document.getElementById('take-quiz-btn');
+async function fetchCategories() {
+  try {
+    const response = await fetch("https://opentdb.com/api_category.php");
+    const data = await response.json();
+    const categories = data.trivia_categories;
+    localStorage.setItem("categories", JSON.stringify(categories));
+    return categories;
+  } catch (error) {
+    console.error("Error fetching trivia categories:", error);
+    throw error; // Re-throw the error to propagate it
+  }
+}
+
+async function setCategories() {
+  if (!localStorage.getItem("categories")) {
+    try {
+      await fetchCategories();
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  }
+
+  const selectElement = document.getElementById("category-select");
+  const categories = JSON.parse(localStorage.getItem("categories"));
+  categories.forEach((category) => {
+    const option = document.createElement("option");
+    option.value = category.id;
+    option.textContent = category.name;
+    selectElement.appendChild(option);
+  });
+}
 
 // Fetch new questions
 takeQuizBtn.addEventListener('click', async function(event) {
